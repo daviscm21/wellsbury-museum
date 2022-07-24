a0dc24686fc13e4043f631138d58a737a19005b4
daviscm21 <davisc1@mail.com>
2022-07-24 18:08:44 +0100

    19

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const app = express(); // create an instance of the express module (app is the conventional variable name used)
 const path = require('path');
 var bodyParser = require('body-parser');
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
 const services = require('./services/requests'); 
 
@@ -11,7 +12,16 @@ const PORT = process.env.PORT || 5000; // use either the host env var port (PORT
 
 app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
 app.use(helmet.noSniff());
-app.use(helmet.contentSecurityPolicy());
+app.use(expressCspHeader({
+  directives: {
+      'default-src': [SELF],
+      'script-src': [SELF, INLINE, 'somehost.com'],
+      'style-src': [SELF, 'mystyles.net'],
+      'img-src': ['data:', 'images.com'],
+      'worker-src': [NONE],
+      'block-all-mixed-content': true
+  }
+}));
 app.use(cors()); // Enable CORS 
 app.use(express.json()); // Recognize Request Objects as JSON objects
 app.use(express.static('client/build')); // serve static files (css & js) from the 'public' directory
